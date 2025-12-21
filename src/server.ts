cd13c0a8b89e78e81000663491ac039c4b1570a2
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-21 17:04:21 +0530

    add express nodemon server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,22 @@
+// console.log(process.env.PORT)
+
+// import config = require("./config");
+// import express from 'express'
+import app from './app.js'
 import { Config } from './config/index.js'
 
 console.log(Config.PORT)
+
+// const app = express()
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => console.log(`Listening on post ${PORT}`))
+    } catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+startServer()
