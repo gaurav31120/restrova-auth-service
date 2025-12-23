950aeca6bce63c015dd3f5f11611ea8db3cd04d6
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-23 09:24:43 +0530

    add jest

---
+++ jest.config.mjs
@@ -0,0 +1,20 @@
+import { createDefaultPreset } from "ts-jest"
+
+const tsJestTransformCfg = createDefaultPreset().transform
+
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
