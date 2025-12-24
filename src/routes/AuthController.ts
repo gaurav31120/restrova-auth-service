4a5f9d7cdcfce3cf765f18fc3795c93a4775214d
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-24 23:31:53 +0530

    testing valid json respnse

--- src/routes/AuthController.ts
+++ src/routes/AuthController.ts
@@ -2,6 +2,7 @@ import { type Request, type Response } from 'express'
 
 export class AuthController {
     register(req: Request, res: Response) {
-        res.status(201).send()
+        // res.status(201).send()
+        res.status(201).json()
     }
 }
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -22,6 +22,27 @@ describe('POST/auth/register', () => {
 
             expect(response.statusCode).toBe(201) // This will fail and show Expected vs Received
         })
+
+        it('should return valid json response', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Gaurav',
+                lastName: 'Kumar',
+                email: 'gaurav@gmail.com',
+                password: 'secret',
+            }
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            // Assert application/json utf-8
+
+            expect(response.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            )
+        })
     })
     describe('Fields are missing', () => {})
 })
