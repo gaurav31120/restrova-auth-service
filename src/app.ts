685ed9a1a72fcd7537b5e2438d2f43c24f686df1
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-24 20:57:01 +0530

    add docker -- containerize the application

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import logger from './config/logger.js'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the restrova project -- auth service')
+    res.send('Welcome to the restrova -- auth service')
 })
 
 // app.get('/', async (req, res, next) => {
