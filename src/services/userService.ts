36cffaa9d304f82001389d77a51961663445a699
Gaurav Kumar <gaurav31120@gmail.com>
2026-01-10 17:30:44 +0530

    add error handling and logging

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,11 +2,31 @@ import type { Repository } from 'typeorm'
 import { AppDataSource } from '../config/data-source.js'
 import { User } from '../entity/User.js'
 import type { UserData } from '../types/index.js'
+import createHttpError from 'http-errors'
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
-    async create({ firstName, lastName, email, password }: UserData) {
+    async create({
+        firstName,
+        lastName,
+        email,
+        password,
+    }: UserData): Promise<User> {
         const userRepository = AppDataSource.getRepository(User)
-        await userRepository.save({ firstName, lastName, email, password })
+
+        try {
+            return await userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            })
+        } catch {
+            const error = createHttpError(
+                500,
+                'Failed to store the data in database',
+            )
+            throw error
+        }
     }
 }
